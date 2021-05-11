4b5de54fa80692e10257cd5f6054265e999cda0c
lanboys <lan_bing2013@163.com>
2021-05-11 16:53:15 +0800

    测试通过数据源DataSource获取连接

---
+++ src/test/java/com/bing/lan/mybatis/jdbc/DataSourceTest.java
@@ -0,0 +1,50 @@
+package com.bing.lan.mybatis.jdbc;
+
+import com.bing.lan.mybatis.dataSource.UnpooledDataSource;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by backend.
+ */
+
+public class DataSourceTest {
+
+  @Test
+  public void testDataSource() {
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      UnpooledDataSource dataSource = new UnpooledDataSource("com.mysql.jdbc.Driver",
+          "jdbc:mysql://14.18.43.48:3306/jdbc", "root", "KHwl20170731!@#");
+      conn = dataSource.getConnection();
+
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("select * from employee");
+      while (rs.next()) {
+        System.out.println(rs.getString("name"));
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    } finally {
+      try {
+        if (rs != null) {
+          rs.close();
+        }
+        if (stmt != null) {
+          stmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
